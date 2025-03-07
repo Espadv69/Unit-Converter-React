74134f60ede16f041d569f2c65551f877e1491a4
Espadv69 <aitorjob69@gmail.com>
2025-03-07 19:31:56 +0100

    Implement input fields and unit selection in Converter component

--- src/components/Converter/Converter.jsx
+++ src/components/Converter/Converter.jsx
@@ -12,6 +12,38 @@ const Converter = () => {
     // toDo: Add logic to convert the input value from one unit to another
     console.log('Converting...')
   }
+
+  return (
+    <section className="converter-section">
+      <input
+        type="number"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Enter value"
+      />
+
+      <div className="selects">
+        <select value={frommUnit} onChange={(e) => setFromUnit(e.target.value)}>
+          <option value="meters">Meters</option>
+          <option value="kilometers">Kilometers</option>
+          <option value="miles">Miles</option>
+          <option value="feet">Feet</option>
+          <option value="inches">Inches</option>
+        </select>
+
+        <span>to</span>
+
+        <select value={toUnit} onChange={(e) => setToUnit(e.target.value)}>
+          <option value="meters">Meters</option>
+          <option value="kilometers">Kilometers</option>
+          <option value="miles">Miles</option>
+          <option value="feet">Feet</option>
+          <option value="inches">Inches</option>
+        </select>
+      </div>
+      {result && <p>Result: {result}</p>}
+    </section>
+  )
 }
 
 export default Converter
