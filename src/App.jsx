7d51062a3bd7d958551d9fa80341ca92fd540268
Espadv69 <aitorjob69@gmail.com>
2025-03-07 19:33:29 +0100

    Add Converter component to App for unit conversion functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './components/Header/Header'
+import Converter from './components/Converter/Converter'
 
 const App = () => {
   return (
     <div className="app">
       <Header />
+      <Converter />
     </div>
   )
 }
