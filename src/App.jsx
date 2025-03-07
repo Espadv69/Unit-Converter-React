2f8418b3ed3c388d06e81ef300ab682fb0c53b30
Espadv69 <aitorjob69@gmail.com>
2025-03-07 19:14:03 +0100

    Add Header component to App for improved layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
-const App = () => {}
+import Header from './components/Header/Header'
+
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  )
+}
 
 export default App
