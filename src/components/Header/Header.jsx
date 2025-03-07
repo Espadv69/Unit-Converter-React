73b19902862e3d211a6e736dfc80581d5718d101
Espadv69 <aitorjob69@gmail.com>
2025-03-07 19:13:09 +0100

    Implement Header component with title and description for unit conversion

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,12 @@
 import './Header.css'
 
-const Header = () => {}
+const Header = () => {
+  return (
+    <header>
+      <h1>Unit Converter</h1>
+      <p>Convert between different units of measurement effortlessly.</p>
+    </header>
+  )
+}
 
 export default Header
