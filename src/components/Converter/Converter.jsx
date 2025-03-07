332f2067104551fdf5da848f7ec6f896d3d82e65
Espadv69 <aitorjob69@gmail.com>
2025-03-07 19:25:36 +0100

    Add conversion logic placeholder in Converter component

--- src/components/Converter/Converter.jsx
+++ src/components/Converter/Converter.jsx
@@ -7,6 +7,11 @@ const Converter = () => {
   const [toUnit, setToUnit] = useState('kilometers')
   const [result, setResult] = useState('')
   // toDo: Add error state
+
+  const handleConvert = () => {
+    // toDo: Add logic to convert the input value from one unit to another
+    console.log('Converting...')
+  }
 }
 
 export default Converter
