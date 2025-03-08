e08a50bbb8dcdd5091d7a670206c2276690a20b7
Espadv69 <aitorjob69@gmail.com>
2025-03-08 08:52:13 +0100

    Add class to header for improved styling and consistency

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <header>
+    <header className="header">
       <h1>Unit Converter</h1>
       <p>Convert between different units of measurement effortlessly.</p>
     </header>
