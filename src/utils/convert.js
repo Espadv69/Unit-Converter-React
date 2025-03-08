4a8683f394ba7fbb21613c2ff180c20149ec878f
Espadv69 <aitorjob69@gmail.com>
2025-03-08 09:09:41 +0100

    Add weight conversion utility function to enhance conversion capabilities

--- src/utils/convert.js
+++ src/utils/convert.js
@@ -6,8 +6,19 @@ export const convertLength = (value, fromUnit, toUnit) => {
     in: 2.54,
     ft: 30.48,
     yd: 91.44,
-    mi: 160934
-  };
+    mi: 160934,
+  }
 
-  return value * units[fromUnit] / units[toUnit];
+  return (value * units[fromUnit]) / units[toUnit]
+}
+
+export const convertWeight = (value, fromUnit, toUnit) => {
+  const units = {
+    g: 1,
+    kg: 1000,
+    lb: 453.592,
+    oz: 28.3495,
+  }
+
+  return (value * units[fromUnit]) / units[toUnit]
 }
