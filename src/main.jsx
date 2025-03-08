22c935573b8beeeec582308a9ce22971293ccc50
Espadv69 <aitorjob69@gmail.com>
2025-03-08 09:53:40 +0100

    Remove unnecessary semicolon from font import statement

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 import './index.css'
-import '@fontsource-variable/roboto';
+import '@fontsource-variable/roboto'
 
 const root = createRoot(document.getElementById('root'))
 
