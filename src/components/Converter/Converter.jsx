2b7f76f275c442901cefaf819f3e90f742cd417f
Espadv69 <aitorjob69@gmail.com>
2025-03-08 08:30:31 +0100

    Fix variable name typo and add input validation in Converter component

--- src/components/Converter/Converter.jsx
+++ src/components/Converter/Converter.jsx
@@ -5,14 +5,24 @@ import './Converter.css'
 
 const Converter = () => {
   const [inputValue, setInputValue] = useState('')
-  const [frommUnit, setFromUnit] = useState('cm')
+  const [fromUnit, setFromUnit] = useState('cm')
   const [toUnit, setToUnit] = useState('m')
   const [result, setResult] = useState('')
   // toDo: Add error state
 
   const handleConvert = () => {
-    // toDo: Add logic to convert the input value from one unit to another
-    console.log('Converting...')
+    if (!inputValue || isNaN(inputValue)) {
+      alert('Please enter a valid number.')
+      return
+    }
+
+    const convertedValue = convertLength(
+      parseFloat(inputValue),
+      fromUnit,
+      toUnit,
+    )
+
+    setResult(convertedValue.toFixed(2))
   }
 
   return (
@@ -25,7 +35,7 @@ const Converter = () => {
       />
 
       <div className="selects">
-        <select value={frommUnit} onChange={(e) => setFromUnit(e.target.value)}>
+        <select value={fromUnit} onChange={(e) => setFromUnit(e.target.value)}>
           <option value="meters">Meters</option>
           <option value="kilometers">Kilometers</option>
           <option value="miles">Miles</option>
